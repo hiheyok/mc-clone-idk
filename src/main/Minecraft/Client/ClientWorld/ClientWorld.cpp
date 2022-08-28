a5c1dec2e4c219e6e1b6f02ad76b52f60b3eac02
hiheyok <longdragonn18@gmail.com>
2022-08-28 01:36:29 -0700

    worked on client / world interaction

--- src/main/Minecraft/Client/ClientWorld/ClientWorld.cpp
+++ src/main/Minecraft/Client/ClientWorld/ClientWorld.cpp
@@ -1,5 +1,32 @@
 #include "ClientWorld.h"
+#include "../../../Utils/Clock.h"
+#include "../../../Utils/LogUtils.h"
+#include "../../../Utils/MathHelper.h"
 
 void ClientWorld::AddChunkServer(Chunk chunk) {
+	ChunkAddQueue.push_back(chunk);
+}
 
+void ClientWorld::Start() {
+	ClientWorldThread = std::thread(&ClientWorld::ClientWorldMainLoop, this);
+}
+
+void ClientWorld::UpdateChunks() {
+	while (!ChunkAddQueue.empty()) {
+		Chunk chunk = ChunkAddQueue.pop_get_front();
+		ChunkCache.insert(getChunkID(chunk.pos), chunk);
+	}
+}
+
+void ClientWorld::ClientWorldMainLoop() {
+	while (!stop) {
+		auto time0 = std::chrono::high_resolution_clock::now();
+		
+		UpdateChunks();
+
+		double time1 = ((std::chrono::high_resolution_clock::now() - time0).count() / 1000000000.0);
+		if (time1 < (1.0 / (double)TPS))
+			timerSleep((1.0 / (double)TPS) - time1);
+		getLogger()->LogDebug("Client Tick", std::to_string(1000000000.0 / (std::chrono::high_resolution_clock::now() - time0).count()));
+	}
 }
--- src/main/Minecraft/Client/ClientWorld/ClientWorld.h
+++ src/main/Minecraft/Client/ClientWorld/ClientWorld.h
@@ -1,15 +1,31 @@
 #pragma once
 
 #include "../../World/Level/Chunk/Chunk.h"
+#include "../../../Utils/MutithreadedData.h"
 
 #include <deque>`
 #include <unordered_map>
+#include <thread>
 
 class ClientWorld {
 public:
+
+	const unsigned int TPS = 20;
+
 	void AddChunkServer(Chunk chunk);
 	void ClientWorldMainLoop();
+	void Start();
 private:
-	std::deque<Chunk> ChunkQueue;
-	std::unordered_map<long long int, Chunk> ChunkCache;
+
+	void UpdateChunks();
+
+	AsyncDeque<Chunk> ChunkAddQueue;
+//	AsyncDeque<???> BlockUpdates; //Work on later
+	AsyncHashMapClass<CHUNK_ID, Chunk> ChunkCache;
+
+	bool stop = false;
+
+	std::thread ClientWorldThread;
+
+	std::unordered_map<char, bool> KeysInputs;
 };
