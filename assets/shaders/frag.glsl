c8577d6175f7d6079e62956d2e2ed391a0351a81
hiheyok <longdragonn18@gmail.com>
2022-09-18 18:06:41 -0700

    better fog

--- assets/shaders/frag.glsl
+++ assets/shaders/frag.glsl
@@ -6,6 +6,7 @@ in float lights;
 in float texturePosition;
 in vec3 FragPos;  
 in vec2 textureSize;
+in vec3 poss;
 
 uniform vec3 camPos;
 uniform float RenderDistance;
@@ -19,27 +20,38 @@ float LinearizeDepth(float depth)
     float z = depth * 2.0 - 1.0; // back to NDC 
     return (2.0 * near * far) / (far + near - z * (far - near));	
 }
-//vec3 skyColor = vec3(0.46274509803f, 0.65882352941f,1.0f,1.0f);
+//
 
 
 void main() 
 {
+	vec3 skyColor = vec3(0.46274509803f, 0.65882352941f,1.0f);
 
-	float depth = 1 - LinearizeDepth(gl_FragCoord.z) / far;
 
-	depth = pow(depth , 4);
 
-	float tsx = textureSize.x;
-	float tsy = textureSize.y;
+	float depth = distance(camPos, poss) / RenderDistance;
 
-	vec4 texture_ =  vec4(texture(BlockTexture, vec3(vec2(tsx,tsy).xy, texturePosition - 1)).rgb , 1);
+	depth = depth * depth;
+	depth = 1 - depth;
+
+
+	vec4 texture_ =  vec4(texture(BlockTexture, vec3(textureSize, texturePosition - 1)).rgb , 1);
 
 	vec4 color = (texture_ * vec4(lights,lights, lights,1.0f));
 
-	float depthr = color.r - ((1 - depth) * (color.r - 0.46274509803f));
-	float depthg = color.g - ((1 - depth) * (color.g - 0.65882352941f));
-	float depthb = color.b - ((1 - depth) * (color.b - 1.0f));
 	
-
-	final = vec4(depthr, depthg, depthb, 1.0f);
+	
+	if (depth < -0.1f) {
+		discard;
+	} else {
+		if (depth < 0.0f) {
+			depth = 0;
+		}
+		
+		float depthr = color.r - ((1 - depth) * (color.r - skyColor.r));
+		float depthg = color.g - ((1 - depth) * (color.g - skyColor.g));
+		float depthb = color.b - ((1 - depth) * (color.b - skyColor.b));
+		final = vec4(depthr, depthg, depthb, 1.0f);
+	}
+	
 }
